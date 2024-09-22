403ad5b364a9f6ba3dd5bd45107e1027be854faf
hcwang24 <hcwang24@student.ubc.ca>
2024-09-22 14:09:00 -0700

    Updating direct model

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -2,6 +2,10 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import numpy as np
+from warnings import simplefilter
+import seaborn as sns
+from matplotlib.ticker import MaxNLocator
 
 
 # A function for plotting the Kp index over a specified date range. 
@@ -141,3 +145,62 @@ def plot_prediction_intervals(
         margin=dict(t=60, b=10),
     )
     return fig
+
+def plot_windows(y, train_windows, test_windows, title=""):
+    """Visualize training and test windows"""
+
+    simplefilter("ignore", category=UserWarning)
+
+    def get_y(length, split):
+        # Create a constant vector based on the split for y-axis."""
+        return np.ones(length) * split
+
+    n_splits = len(train_windows)
+    n_timepoints = len(y)
+    len_test = len(test_windows[0])
+
+    train_color, test_color = sns.color_palette("colorblind")[:2]
+
+    fig, ax = plt.subplots(figsize=plt.figaspect(0.3))
+
+    for i in range(n_splits):
+        train = train_windows[i]
+        test = test_windows[i]
+
+        ax.plot(
+            np.arange(n_timepoints), get_y(n_timepoints, i), marker="o", c="lightgray"
+        )
+        ax.plot(
+            train,
+            get_y(len(train), i),
+            marker="o",
+            c=train_color,
+            label="Train",
+        )
+        ax.plot(
+            test,
+            get_y(len_test, i),
+            marker="o",
+            c=test_color,
+            label="Validation",
+        )
+    ax.invert_yaxis()
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.set(
+        title=title,
+        ylabel="Window number",
+        xlabel="Time",
+    )
+    # remove duplicate labels/handles
+    handles, labels = [(leg[:2]) for leg in ax.get_legend_handles_labels()]
+    ax.legend(handles, labels);
+    
+def get_windows(y, cv):
+    """Generate windows"""
+    train_windows = []
+    test_windows = []
+    for i, (train, test) in enumerate(cv.split(y)):
+        train_windows.append(train)
+        test_windows.append(test)
+    return train_windows, test_windows
+
